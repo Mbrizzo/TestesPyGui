03b027737b764a3d018c2e126dfd8f4ba6928443
Marcel <mrizzo25@gmail.com>
2023-05-08 23:09:35 -0300

    função principal e theme

---
+++ appScrap/main.py
@@ -0,0 +1,8 @@
+import PySimpleGUI as sg # Interface Gráfica
+
+def main():
+    sg.theme('Reddit') # https://www.geeksforgeeks.org/themes-in-pysimplegui/
+
+
+if __name__ == '__main__':
+    main()
